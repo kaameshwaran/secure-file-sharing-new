104dfa25f563149e188753dcd76bbe87326e2279
Kaameshwaran V <727721euec062@skcet.ac.in>
2025-01-09 15:21:26 +0530

    set up routing to home and upload pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import FileList from '../components/FileList';
+
+const HomePage = () => {
+  return <FileList />;
+};
+
+export default HomePage;
---
+++ src/pages/UploadPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import FileUploader from '../components/FileUploader';
+
+const UploadPage = () => {
+  return <FileUploader />;
+};
+
+export default UploadPage;
